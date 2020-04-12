3574ace4d23852babf1814500e7213dcc82100e6
DipaliBachhav <dipa2718@gmail.com>
2020-04-12 12:36:57 +0530

    Refactor Using Switch Case

--- empWage.sh
+++ empWage.sh
@@ -2,22 +2,23 @@
 
 echo "Welcome To Employee Wage Computation On Master Branch"
 
-#Calculating Part Time Wage Of Employee
-
 isPartTime=1
 isFullTime=2
 empWagePerHr=20
-randomCheck=$(( RANDOM%3 ))
+empCheck=$(( RANDOM%3 ))
 
-if [[ $isFullTime -eq $randomCheck ]]
-then
-	empHrs=8
-elif [[ $isPartTime -eq $randomCheck ]]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+case $empCheck in
+	$isFullTime)
+		empHrs=8
+		;;
 
+	$isParTime)
+		empHrs=4
+		;;
+
+	*)
+		empHrs=0
+		;;
+esac
 salry=$(( $empHrs*$empWagePerHr ))
 
