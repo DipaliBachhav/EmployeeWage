db4f2dc56f0b4c10dd0ab34304a353a3593605c2
DipaliBachhav <dipa2718@gmail.com>
2020-04-12 11:13:51 +0530

    Added Employee Attendence To File

--- empWage.sh
+++ empWage.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
-echo "Welcome To Employee Wage Computation Program On Master Branch"
+
+#Calculating Employee isPresent Or Not
+
+isPresent=1
+randomCheck=$(( RANDOM%2 ))
+
+if [[ $isPresent -eq $randomCheck ]]
+then
+	echo "Employee Is Present"
+else
+	echo "Employee Is Absent"
+fi
 
