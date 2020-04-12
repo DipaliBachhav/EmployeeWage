8cb25b98af72876b4b8e5a03db79a749eb1e3995
DipaliBachhav <dipa2718@gmail.com>
2020-04-12 13:36:28 +0530

    Added Employee Wages For Month

--- empWage.sh
+++ empWage.sh
@@ -4,9 +4,13 @@ echo "Welcome To Employee Wage Computation On Master Branch"
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
+numWorkingDays=20
 empWagePerHr=20
-empCheck=$(( RANDOM%3 ))
 
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+empCheck=$(( RANDOM%3 ))
 case $empCheck in
 	$isFullTime)
 		empHrs=8
@@ -20,5 +24,6 @@ case $empCheck in
 		empHrs=0
 		;;
 esac
-salry=$(( $empHrs*$empWagePerHr ))
-
+	salary=$(( $empHrs*$empWagePerHr ))
+	totalSalary=$(( $totalSalary+$salary ))
+done
